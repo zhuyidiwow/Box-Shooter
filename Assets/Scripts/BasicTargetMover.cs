d5f26640de6191779c682f523ddc2d64682afdbb
AndyZhu <zhuyidiwow@gmail.com>
2016-09-18 15:17:50 +0800

    Scene and UI setup but UI not working

---
+++ Assets/Scripts/BasicTargetMover.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicTargetMover : MonoBehaviour {
+
+    public float spinSpeed = 180f;
+	public float motionMagnitude = 0.1f;
+
+    public bool doSpin = true;
+    public bool doMotion = false;
+
+	void Update () {
+        // rotate around the up axis of the game object
+        if (doSpin) {
+            gameObject.transform.Rotate(Vector3.up * spinSpeed * Time.deltaTime); // scaling by time
+        }
+        // move up and down over time
+        if (doMotion) {
+            gameObject.transform.Translate(Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * motionMagnitude);
+        }
+    }
+}
